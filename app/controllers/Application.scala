ed7ef5d1763e163a7c6c7c1d267a0861c1587217
jzw <johnw@sfwltd.co.uk>
2016-07-04 11:55:29 +0100

    Demo

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,12 +4,21 @@ import play.api._
 import play.api.mvc._
 import play.api.cache.Cache
 import play.api.Play.current
+import org.jscience.physics.model.RelativisticModel
+import org.jscience.physics.amount.Amount
+import javax.measure.unit.SI.KILOGRAM
+import javax.measure.quantity.Mass
 
 import play.api.db._
 
 object Application extends Controller {
 
   def index = Action {
+    RelativisticModel.select()
+    val m = Amount.valueOf("12 GeV").to(KILOGRAM)
+    val testRelativity = s"E=mc^2: 12 GeV = $m"
+    Ok(views.html.index(testRelativity))
+
     Ok(views.html.index(null))
   }
 
